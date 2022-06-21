01106e81029a8a6556f148498ed9a5db2e236f70
Azaz133 <100973712+Azaz133@users.noreply.github.com>
2022-06-21 20:00:05 +0300

    Box Collider to player
    Box collider to player and player component

---
+++ 3D oyun projesi/Assets/Game Folders/Script/Concretes/Controllers/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project3D.controllers
+{
+
+}
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    
+}
