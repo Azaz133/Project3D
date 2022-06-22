15909d1a7766454ac0945ae55721a1bf2edef4a6
Azaz133 <100973712+Azaz133@users.noreply.github.com>
2022-06-22 07:19:07 +0300

    A few changes
    component

--- 3D oyun projesi/Assets/Game Folders/Script/Concretes/Controllers/PlayerController.cs
+++ 3D oyun projesi/Assets/Game Folders/Script/Concretes/Controllers/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,24 @@ using UnityEngine;
 namespace Project3D.controllers
 {
 
+    public class PlayerController : MonoBehaviour 
+    {
+        private Rigidbody rigidbody; 
+        private void Awake()
+        {
+            rigidbody GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
 
 
-
-public class PlayerController : MonoBehaviour
-{
-    
-}
